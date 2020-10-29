3558d39b4d26af503b1b67a986aa6ccde742f6b8
Sahil Davawala <Sahil.Davawala@sapienceanalytics.com>
2020-10-29 11:08:47 +0000

    Updated ps_prometheus_alertmanager.sh

--- ps_prometheus_alertmanager.sh
+++ ps_prometheus_alertmanager.sh
@@ -1,7 +1,7 @@
 echo "Get access credentials for the Production AKS."
 az aks get-credentials --subscription 43e0bf01-5025-40ce-bdaa-c4291177828a --resource-group prod-us --name prod-us
 
-echo "The Prometheus UI can be seen by visiting: http://localhost:9093/#/alerts"
+echo "The Prometheus-AlertManager UI can be seen by visiting: http://localhost:9093/#/alerts"
 kubectl --kubeconfig .local/kubeconfig port-forward `. get_one_pod_name_by_ns_and_label.sh monitoring app=alertmanager` -n monitoring 9093
 
 echo "Deleting the Production context from kubectl config to avoid any discrepancies"
