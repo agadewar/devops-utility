d87ff97ae415e50b44663a7a5cf3fbd223133338
Ashish Gadewar <Ashish.Gadewar@sapienceanalytics.com>
2021-05-21 13:31:13 +0000

    Updated common-sql-queries.sql

--- common-sql-queries.sql
+++ common-sql-queries.sql
@@ -1,5 +1,5 @@
-//select reports with the name
+---select reports with the name
 select * from org.reportcataloggeneral where reportname like 'My Work Habit'
 
-//Update report, active imactive
+---Update report, active imactive
 update org.reportcataloggeneral set isactive = 0 where reportname like 'My Work Habit'.  
